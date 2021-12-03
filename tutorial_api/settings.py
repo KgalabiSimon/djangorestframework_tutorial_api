428db5daa5bce6120aa2bfb1bca85d95738189de
KgalabiSimon <54315034@mylife.unisa.ac.za>
2021-12-03 22:59:04 +0200

    update snippets/url.py

--- tutorial_api/settings.py
+++ tutorial_api/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'snippets.apps.SnippetsConfig',
 ]
 
+ROOT_URLCONF = 'snippets.urls'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
