2982e1516f857af88eeea2ff6a125022f3b27424
KgalabiSimon <54315034@mylife.unisa.ac.za>
2021-12-04 17:56:09 +0200

    updated snippet/views.py by using the Response object and status code.Added forma_suffix_patterns snippet.urls.py

--- snippets/urls.py
+++ snippets/urls.py
@@ -3,6 +3,8 @@ from rest_framework.routers import DefaultRouter
 
 from snippets import views
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+
 
 # Create a router and register our viewsets with it.
 
@@ -12,3 +14,5 @@ urlpatterns = [
     path('snippets/', views.snippet_list),
     path('snippets/<int:pk>/', views.snippet_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
